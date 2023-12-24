41650a37a08d382bf6f8396374a31dc931538ffd
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-24 17:50:29 +0530

    Update graph.py

--- plugins/graph.py
+++ plugins/graph.py
@@ -5,13 +5,12 @@ from telegraph import upload_file, Telegraph
 
 DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./Downloads/")
 
-
 @Client.on_message(filters.media & filters.private)
 async def getmedia(bot, update):
     medianame = DOWNLOAD_LOCATION + str(update.from_user.id)
     try:
         message = await update.reply(
-            text="`Processing...`",
+            text="**Processing...**",
             quote=True,
             disable_web_page_preview=True
         )
@@ -26,7 +25,7 @@ async def getmedia(bot, update):
             print(remove_error)
     except Exception as error:
         print(error)
-        text = f"Error: <code>{error}</code>"
+        text = f"**Error :-** <code>{error}</code>"
         reply_markup = InlineKeyboardMarkup(
             [[
                 InlineKeyboardButton('More Help', callback_data='help')
@@ -39,13 +38,13 @@ async def getmedia(bot, update):
         )
         return
 
-    text = f"**Link:** `https://graph.org{response[0]}`\n\n**Join:** @dxmodsupdates"
+    text = f"**Link :- https://graph.org{response[0]}**\n\n**Join :- [Star Bots Tamil](https://t.me/Star_Bots_Tamil)**"
     reply_markup = InlineKeyboardMarkup(
         [[
             InlineKeyboardButton(text="Open Link", url=f"https://graph.org{response[0]}"),
             InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
         ], [
-            InlineKeyboardButton(text="Join Updates Channel", url="https://telegram.me/dxmodsupdates")
+            InlineKeyboardButton(text="Join Updates Channel", url="https://t.me/Star_Bots_Tamil")
         ]]
     )
     await message.edit_text(
@@ -77,7 +76,7 @@ async def text_handler(bot, update):
         except Exception as e:
             print(e)
 
-        await update.reply_text("https://graph.org/{}".format(response["path"]))
+        await update.reply_text("**https://graph.org/{}**".format(response["path"]))
 
     except Exception as e:
         print(e)
